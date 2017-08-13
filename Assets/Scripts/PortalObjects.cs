c5b6d2ee461b68ce5a2d4734798e4749e211cf14
IzikArruda <izikarruda@gmail.com>
2017-08-13 12:35:55 -0300

    Properly formatted execution flow
    The process of execution when handling portal creation is now all done from the PortalSet by having it call the linked portalObject's Setter functions.

--- Assets/Scripts/PortalObjects.cs
+++ Assets/Scripts/PortalObjects.cs
@@ -2,103 +2,57 @@
 using System.Collections;
 
 /*
- * Attach this to the game object that parents the objects of a singular portal. It will link to
- * the portal's mesh and door objects.
- * 
- * portalMesh being the gameObject that contains the MeshRenderer
+ * Holds links to all relevent gameObjects for a portal, such as the triggers, the mesh and the containers.
+ * It is often called by it's parent, an empty transform with a PortalSet script.
  */
-[ExecuteInEditMode]
 public class PortalObjects : MonoBehaviour {
 
+    /* The gameObject that contains the portal's mesh and PortalView script */
     public GameObject portalMesh;
+
+    /* The two triggers that the player will teleport between for this portal */
     public GameObject teleporterEnterTrigger;
     public GameObject teleporterLeaveTrigger;
-    //public GameObject portalDoor;
-
-    /* The sizes of the portal mesh */
-    public float portalWidth;
-    public float portalHeight;
-    /* positional offset of the portal */
-    public Vector3 portalOffset;
-
-    public Vector3 wierdMeshOffset;
-
-    /* A link to the border that surrounds the contour of the portal mesh. */
-    public GameObject border;
-
-
+    
     /* A link to each child container of the portal */
     public Transform meshContainer;
     public Transform TriggerContainer;
     public Transform borderContainer;
 
+    /* A link to the border that surrounds the contour of the portal mesh. */
+    private GameObject border;
 
 
-    /* Update the sizes of the mesh of the given portals using the saved sizes */
-    public void CreatePortalMesh() {
+    /* -------- Setters ---------------------------------------------------- */
+
+    public void SetMesh(Mesh mesh) {
         /*
-         * Create the portal mesh that the player will be viewing using the portal's size parameters.
-         * 
-         * A portal mesh's (0, 0, 0) will be on the mesh's bottom center.
+         * Link the given mesh to the meshRenderer that is used to dispaly the portal.
+         */
+         
+        portalMesh.GetComponent<MeshFilter>().mesh = mesh;
+    }
+    
+    public void SetTriggersSizes(float width, float height, float depth) {
+        /*
+         * Set the position, rotation and scale of the portal's triggers using the given parameters.
          */
 
-        ////NOTE: THE PARTNER WILL ALOS NEED TO HAVE IT'S MESH CHANGED
-
-        Mesh mesh = new Mesh();
-        Vector3[] vertices;
-        Vector2[] UV;
-        int[] triangles;
-
-        /* Set the vertices for the portal mesh */
-        //NOTE: WHY DO I NEED TO REMOVE 0.25F to allign the meshes textures?
-        vertices = new Vector3[] {
-                new Vector3(-portalWidth, portalHeight, 0) + portalOffset,
-                new Vector3(0, portalHeight, 0) + portalOffset,
-                new Vector3(0, 0, 0) + portalOffset,
-                new Vector3(-portalWidth, 0, 0) + portalOffset
-            };
-
-        /* Set the two polygons that form the portal mesh */
-        triangles = new int[] {
-                3, 2, 1, 3, 1, 0
-            };
-
-        /* Set the UVs for the portal mesh */
-        UV = new Vector2[] {
-                new Vector2(0, 0),
-                new Vector2(0, 1),
-                new Vector2(1, 0),
-                new Vector2(1, 1)
-            };
-
-        /* Assign the mesh to renderer */
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
-        //mesh.uv = UV;
-        portalMesh.GetComponent<MeshFilter>().mesh = mesh;
-
-
-
-        //The thickness of the portal should be a small amout larger than twice the camera's near clipping plane
-        //The Offset of the portal should be slightly larger than the camera's near clipping plane instead
-        float portalThickness = 0.01f;
-        /*  Set the teleport triggers linked to the portal to be the same sizes of the portal mesh */
+        /* Set the properties of this script's portal's trigger */
         teleporterEnterTrigger.transform.localEulerAngles = new Vector3(0, 0, 0);
         teleporterEnterTrigger.transform.localScale = transform.localScale;
-        teleporterEnterTrigger.transform.localPosition = new Vector3(-portalWidth/2f, portalHeight/2f, 0);
+        teleporterEnterTrigger.transform.localPosition = new Vector3(-width/2f, height/2f, 0);
         teleporterEnterTrigger.GetComponent<BoxCollider>().center = new Vector3(0, 0, 0);
-        teleporterEnterTrigger.GetComponent<BoxCollider>().size = new Vector3(portalWidth, portalHeight, portalThickness);
+        teleporterEnterTrigger.GetComponent<BoxCollider>().size = new Vector3(width, height, depth);
 
+        /* Set the properties of the trigger at the partner portal */
         teleporterLeaveTrigger.transform.localEulerAngles = new Vector3(0, 0, 0);
         teleporterLeaveTrigger.transform.localScale = transform.localScale;
-        teleporterLeaveTrigger.transform.localPosition = new Vector3(-portalWidth/2f, portalHeight/2f, 0); ;
+        teleporterLeaveTrigger.transform.localPosition = new Vector3(-width/2f, height/2f, 0); ;
         teleporterLeaveTrigger.GetComponent<BoxCollider>().center = new Vector3(0, 0, 0);
-        teleporterLeaveTrigger.GetComponent<BoxCollider>().size = new Vector3(portalWidth, portalHeight, portalThickness);
-
-        
+        teleporterLeaveTrigger.GetComponent<BoxCollider>().size = new Vector3(width, height, depth);
     }
-
-
+    
     public void SetBorder(GameObject borderObject) {
         /*
          * Set the border of the portal to be the given gameObject. Duplicate the given object and 
--- Assets/Scripts/PortalSet.cs
+++ Assets/Scripts/PortalSet.cs
@@ -2,8 +2,10 @@
 using System.Collections;
 
 /*
- * A set of portalObjects that will be linked together. Both portalObjects will be treated the same.
+ * A set of portalObjects that will be linked together. This script calls their Setter functions to 
+ * properly set the portal's parameters (mesh, triggers, borders) to be identical.
  */
+[ExecuteInEditMode]
 public class PortalSet : MonoBehaviour {
     
     /* The two portals that are linked together */
@@ -12,27 +14,70 @@ public class PortalSet : MonoBehaviour {
 
     /* The object used as a border for the portal mesh. Leave it empty for a default border */
     public GameObject portalBorder;
-    
+
+    /* The mesh of the portal. If this is null, then the defaultPortalSize will be used to create the mesh */
+    public Mesh portalMesh;
+    public float defaultWidth;
+    public float defaultHeight;
+
+    /* positional offset of the portal */
+    public Vector3 portalOffset;
+
+    /* Depth of the portal's trigger. The higher value, the more noticable of a jump will occur on teleport */
+    public float portalThickness;
+
+
+    /* -------- Built-In Unity Functions ---------------------------------------------------- */
 
     void Update() {
 
-        /* Create the meshes of the portals */
+        /* Create and link the meshes of the portals */
         CreateMesh();
+        
+        /* Fix the transform of the portal's triggers */
+        UpdateTriggers();
+    }
 
-        /* Create the borders around each linked portal */
+    void Start() {
+        /*
+         * Leave the border creation in the start function until we start to work on borders.
+         */
+
+        /* Create and link the borders around each linked portal */
         CreateBorder();
     }
 
+
+    /* -------- Update Functions ---------------------------------------------------- */
+
+    void UpdateTriggers() {
+        /*
+         * Adjust the position, rotation and scale of the triggers.
+         */
+
+        EntrancePortal.SetTriggersSizes(defaultWidth, defaultHeight, portalThickness);
+        ExitPortal.SetTriggersSizes(defaultWidth, defaultHeight, portalThickness);
+    }
+    
     void CreateMesh() {
         /*
          * Create the mesh for the linked portals using the given portalMesh. If there is no portalMesh,
-         * default to use a generated mesh with the given defaultMeshSize values
+         * use a generated mesh with the given defaultMeshSize values.
          */
+        Mesh mesh;
 
-        EntrancePortal.CreatePortalMesh();
-        ExitPortal.CreatePortalMesh();
+        /* Get either the linked mesh for the portal or create the the default */
+        if(portalMesh) {
+            mesh = portalMesh;
+        }else {
+            mesh = CreateDefaultMesh();
+        }
+
+        /* Assign the mesh to each linked portalObject */
+        EntrancePortal.SetMesh(mesh);
+        ExitPortal.SetMesh(mesh);
     }
-
+    
     void CreateBorder() {
         /*
          * Create a border for each portal. They will have the same border to ensure consistensy between portals.
@@ -46,5 +91,36 @@ public class PortalSet : MonoBehaviour {
         else {
             Debug.Log("CREATING A DEFAULT BORDER");
         }
+    }
+
+
+    /* -------- Event Functions ---------------------------------------------------- */
+
+    Mesh CreateDefaultMesh() {
+        /*
+         * Create the default mesh for the portal using the default width and height. It does not need UVs
+         */
+        Mesh defaultMesh = new Mesh();
+        Vector3[] vertices;
+        int[] triangles;
+
+        /* Set the vertices for the mesh */
+        vertices = new Vector3[] {
+                new Vector3(-defaultWidth, defaultHeight, 0) + portalOffset,
+                new Vector3(0, defaultHeight, 0) + portalOffset,
+                new Vector3(0, 0, 0) + portalOffset,
+                new Vector3(-defaultWidth, 0, 0) + portalOffset
+            };
+
+        /* Set the two polygons that form the mesh */
+        triangles = new int[] {
+                3, 2, 1, 3, 1, 0
+        };
+
+        /* Assign the properties of the mesh */
+        defaultMesh.vertices = vertices;
+        defaultMesh.triangles = triangles;
+
+        return defaultMesh;
     }
 }
