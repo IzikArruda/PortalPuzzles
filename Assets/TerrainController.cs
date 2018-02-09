c8b9488d513957d2585855bcbec12e3871681e77
IzikArruda <izikarruda@gmail.com>
2018-02-09 08:08:10 -0400

    Added chunk generation in groups, along with a way to get all chunks within a radius
    The game can now generate a group of chunks around the given position with the given radius. This is done using a vector2 to represent each chunk and will allow easy access to chunks later when using list functions.

--- Assets/TerrainController.cs
+++ Assets/TerrainController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 /*
  * Used to control the creation of terrain using the TerrainChunk script.
@@ -9,16 +10,17 @@ public class TerrainController : MonoBehaviour {
 
     void Start() {
         /*
-         * For now, just create a single random terrain
+         * Create a large circle of terrain around the origin
          */
-         
-        CreateTerrainChunk();
-    }
-    
 
-    void CreateTerrainChunk() {
+        List<Vector2> newChunks = GetVisibleChunksFromPosition(new Vector2(0, 0), 5);
+        CreateTerrainChunks(newChunks);
+    }
+
+
+    void CreateTerrainChunk(int x, int z) {
         /*
-         * Create a single chunk of terrain 
+         * Create a single chunk of terraingiven the coordinates of the terrain 
          */
 
         /* Create a new object and attach a TerrainChunk and it's settings */
@@ -30,6 +32,38 @@ public class TerrainController : MonoBehaviour {
 
         /* Link the settings to the terrain chunk and generate new terrain */
         newChunk.LinkSettings(newChunkSettings);
-        newChunk.GenerateTerrain(0, 0);
+        newChunk.GenerateTerrain(x, z);
+    }
+
+    void CreateTerrainChunks(List<Vector2> chunks) {
+        /*
+         * Create each chunk described by the given list
+         */
+
+        foreach(Vector2 chunk in chunks) {
+            CreateTerrainChunk((int)chunk.x, (int)chunk.y);
+        }
+    }
+
+
+
+
+    private List<Vector2> GetVisibleChunksFromPosition(Vector2 chunkPosition, int radius) {
+        /*
+         * Return a list of all chunks that should be rendered given the position.
+         * The position and radius values are relative to chunk sizes.
+         */
+        List<Vector2> visibleChunks = new List<Vector2>();
+
+        /* Add each visible chunk to the chunk list */
+        for(int x = -radius; x < radius; x++) {
+            for(int z = -radius; z < radius; z++) {
+                if(x*x + z*z < radius*radius) {
+                    visibleChunks.Add(new Vector2(chunkPosition.x + x, chunkPosition.y + z));
+                }
+            }
+        }
+
+        return visibleChunks;
     }
 }
