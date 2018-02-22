9146636e4792cabb7908505a8e44d7c7fc10eaf9
IzikArruda <izikarruda@gmail.com>
2018-02-22 14:00:22 -0400

    Adjusted how the skySphere is rendered for the player camera
    The SkySphere is now rendered through a different camera for the player. By adding a script to the object that holds a camera, we can make that camera ignore the skybox and have another camera as a child that will render the skySphere. This new camera can then have sepperate effects, such as ignore the fog.

---
+++ Assets/SkySphereCamera.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Attach this sctipr to an object that contains a camera that will use the skysphere
+ */
+public class SkySphereCamera : MonoBehaviour {
+
+    /* The camera that will render only the skySphere */
+    private GameObject skySphereCameraObject;
+    private Camera skysphereCamera;
+
+
+	void Start () {
+        /*
+         * On startup, create the camera and set it's properties to make it only render the skySphere
+         */
+
+        /* Create the object to hold the camera */
+        skySphereCameraObject = new GameObject();
+        skySphereCameraObject.name = "Sky Sphere Camera";
+        skySphereCameraObject.layer = PortalSet.maxLayer + 1;
+        skySphereCameraObject.transform.parent = transform;
+        skySphereCameraObject.transform.localPosition = new Vector3(0, 0, 0);
+        skySphereCameraObject.transform.localEulerAngles = new Vector3(0, 0, 0);
+        skySphereCameraObject.transform.localScale = new Vector3(1, 1, 1);
+
+        /* Attach the camera and change it's settings */
+        skysphereCamera = skySphereCameraObject.AddComponent<Camera>();
+        skysphereCamera.clearFlags = CameraClearFlags.Depth;
+        skysphereCamera.cullingMask = 1 << PortalSet.maxLayer + 1;
+
+        /* Get the camera that wants the skySphere to be rendered and use some of it's stats */
+        if(GetComponent<Camera>() != null) {
+            skysphereCamera.depth = GetComponent<Camera>().depth - 1;
+            skysphereCamera.nearClipPlane = GetComponent<Camera>().nearClipPlane;
+            skysphereCamera.farClipPlane = GetComponent<Camera>().farClipPlane;
+
+            /* Do not let the camera render the skySphere */
+            GetComponent<Camera>().clearFlags = CameraClearFlags.Nothing;
+            GetComponent<Camera>().cullingMask = ~(1 << PortalSet.maxLayer + 1);
+        }
+    }
+}
