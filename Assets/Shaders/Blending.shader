6a07b0ee79a6b4b32369238ad9422ff9f94ab74b
IzikArruda <izikarruda@gmail.com>
2018-03-08 14:40:14 -0400

    Added a script to update the shader's values
    The shader now has two float values that control when and for how long it takes the texture to blend. It can even use a focus object to alter where the blending begins. The shader is comming along nicely and in the testScene, it seems as if the focus point serves as a light.

--- Assets/Shaders/Blending.shader
+++ Assets/Shaders/Blending.shader
@@ -3,7 +3,8 @@
 		_MainTex("Base (RGB)", 2D) = "white"
 		_BlendTex("Blend (RGB)", 2D) = "black"
 		_CameraPos("CamPos", Vector) = (0, 0, 0, 0)
-		_Radius ("Radius", Float) = 1
+		_BlendDistance("BlendDistance", Float) = 0
+		_BlendRate ("BlendRate", Float) = 0
 	}
 
 	SubShader{
@@ -17,15 +18,16 @@
 		};
 		sampler2D _MainTex;
 		sampler2D _BlendTex;
-		float3 _CamPos;
-		float _Radius;
+		float3 _CameraPos;
+		float _BlendDistance;
+		float _BlendRate;
 
 		void surf(Input IN, inout SurfaceOutput o) {
 			/* Get the distance between the camera's position and the pixel's point */
-			float d = distance(_CamPos, IN.worldPos);
-			float dN = 1 - saturate(d / _Radius);
+			float d = distance(_CameraPos, IN.worldPos);
+			float dN = 1 - saturate((d - _BlendDistance) / (_BlendRate));
 
-			dN = step(dN, 0);
+			//dN = step(dN, 0);
 			//dN = step(dN, 1);
 			o.Albedo = tex2D(_MainTex, IN.uv_MainTex).rgb*(dN) +tex2D(_BlendTex, IN.uv_BlendTex).rgb*(1 - dN);
 		}
---
+++ Assets/UpdateBlendShader.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Update the value of the texture blending shader each frame
+ */
+public class UpdateBlendShader : MonoBehaviour {
+
+    public GameObject focusObject;
+
+	void Start() {
+        /*
+         * Set the radius of the shader that specifies how far the texture needs to be
+         * for the texture to fully blend.
+         */
+
+        /* Set the radius of when the texture starts blending */
+        GetComponent<Renderer>().material.SetFloat("_BlendDistance", 1f);
+
+        /* Set the distance of how far it takes for the texture to start blending */
+        GetComponent<Renderer>().material.SetFloat("_BlendRate", 5f);
+    }
+
+	void Update () {
+        /*
+         * Update the values within the connected renderer's blending shader
+         */
+
+        /* Update the shader's camera position with this script's focus object */
+        if(focusObject != null) {
+            GetComponent<Renderer>().material.SetVector("_CameraPos", focusObject.transform.position);
+        }
+	}
+}
