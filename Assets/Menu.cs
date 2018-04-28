0f9a476db6bcc8101b159feca02f01717b757a3d
IzikArruda <izikarruda@gmail.com>
2018-04-28 15:35:52 -0300

    Added an animation to the button when clicked. Added MouseHover for start button
    When the button is pressed, a variable gets ticked and an animation on the button starts to make it fade away. This effect does not look great, but the format of an animation is now in place.
    
    Also, the event of the mouse hovering over the start button is now caught and handled using event triggers for the button.

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using System.Collections;
 
 /*
@@ -21,27 +23,52 @@ public class Menu : MonoBehaviour {
     /* The main "Start" button */
     public Button startButton;
     public RectTransform startButtonRect;
+    private bool animatingStartButton = false;
+    private float startButtonRemainingTime;
+
+
+    /* Previous resolutions of the window */
+    public float screenWidth;
+    public float screenHeight;
+
+
+    /* ----------- Built-in Functions ------------------------------------------------------------- */
+
+    void Update() {
+        /*
+         * Check if there was a change in the current window size
+         */
+
+        if(screenWidth != Screen.width || screenHeight != Screen.height) {
+
+            /* Update the new sizes and update the menu's positions */
+            screenWidth = Screen.width;
+            screenHeight = Screen.height;
+            UpdatePositions();
+        }
+
+        /* Update the start button's visuals */
+        if(animatingStartButton == true) {
+            startButtonRemainingTime -= Time.deltaTime;
+            UpdateStartButtonVisuals();
+        }
+    }
 
 
     /* ----------- Set-up Functions ------------------------------------------------------------- */
+
 
     public void Initialize(CustomPlayerController controller) {
         /*
          * Sets up the main menu. Requires a link to the playerController to add functionallity to the buttons
          */
-        playerController = controller;
 
-        /* Setup the canvas */
+        /* Link the global variables of the script */
+        playerController = controller;
         canvasRect = canvas.GetComponent<RectTransform>();
 
-        /* Setup the start button */
-        startButton.onClick.AddListener(StartButtonClick);
-        startButtonRect = startButton.GetComponent<RectTransform>();
-        startButtonRect.sizeDelta = new Vector2(300, 100);
-        startButtonRect.position = new Vector3(1.1f*startButtonRect.sizeDelta.x/2f, canvasRect.position.y, 0);
-        startButton.GetComponentInChildren<Text>().text = "START";
-        startButton.GetComponentInChildren<Text>().fontSize = 60;
-        SetupText(startButton.GetComponentInChildren<Text>());
+        /* Run the initialSetup functions for the buttons */
+        SetupStartButton();
     }
 
     public void SetupText(Text text) {
@@ -53,11 +80,82 @@ public class Menu : MonoBehaviour {
         text.font = usedFont;
         text.fontStyle = FontStyle.Normal;
 
-        /* Set the outline */
-        if(text.GetComponent<Outline>() == null) { text.gameObject.AddComponent<Outline>(); }
-        text.GetComponent<Outline>().effectColor = new Color(0, 0, 0, 170);
-        text.GetComponent<Outline>().effectDistance = new Vector2(2, 2);
+        /* Set the outlines for the text. There will be two outlines used for each text. */
+        if(text.gameObject.GetComponent<Outline>() == null) { text.gameObject.AddComponent<Outline>(); text.gameObject.AddComponent<Outline>(); }
+        Outline[] outlines = text.gameObject.GetComponents<Outline>();
     }
+
+    public void SetupStartButton() {
+        /*
+         * Set the values and variables needed for the start button
+         */
+
+        /* Get the components used on the button */
+        startButton.onClick.AddListener(StartButtonClick);
+        startButtonRect = startButton.GetComponent<RectTransform>();
+
+        /* Add an event trigger for when the mosue hovers over */
+        EventTrigger startButtonTrigger = startButton.gameObject.AddComponent<EventTrigger>();
+        EventTrigger.Entry startButtonEntry = new EventTrigger.Entry();
+        startButtonEntry.eventID = EventTriggerType.PointerEnter;
+        startButtonEntry.callback.AddListener((data) => { StartButtonMouseEnter(); });
+        startButtonTrigger.triggers.Add(startButtonEntry);
+
+        /* Set the sizes and content of the button */
+        SetupText(startButton.GetComponentInChildren<Text>());
+        startButtonRect.sizeDelta = new Vector2(400, 125);
+        startButton.GetComponentInChildren<Text>().text = "START";
+        startButton.GetComponentInChildren<Text>().fontSize = 100;
+        
+        /* Update the visuals of the button */
+        UpdateStartButtonVisuals();
+    }
+
+
+    /* ----------- Visual Functions ------------------------------------------------------------- */
+
+    public void UpdateStartButtonVisuals() {
+        /*
+         * Update the visuals of the start button. This includes the outlines used. Adjust the visuals
+         * depending on whether animatingStartButton is true or not and how much startButtonRemainingTime is left.
+         */
+
+        Outline[] outlines = startButton.GetComponentInChildren<Text>().gameObject.GetComponents<Outline>();
+
+        /* Leave the button as normal */
+        if(animatingStartButton == false) {
+            startButton.GetComponentInChildren<Text>().color = new Color(1, 1, 1, 1);
+            outlines[0].effectColor = new Color(0, 0, 0, 0.3f);
+            outlines[0].effectDistance = new Vector2(1.5f, 1.5f);
+            outlines[1].effectColor = new Color(0, 0, 0, 0.3f);
+            outlines[1].effectDistance = new Vector2(1.5f, 1.5f);
+        }
+
+        /* Animate the button fading away */
+        else {
+            float ratio = startButtonRemainingTime/5f;
+            if(ratio < 0) { ratio = 0; }
+            Debug.Log(ratio);
+
+            startButton.GetComponentInChildren<Text>().color = new Color(1, 1, 1, ratio*1);
+            outlines[0].effectColor = new Color(0, 0, 0, ratio*0.3f);
+            outlines[0].effectDistance = new Vector2(1.5f, 1.5f);
+            outlines[1].effectColor = new Color(0, 0, 0, ratio*0.3f);
+            outlines[1].effectDistance = new Vector2(1.5f, 1.5f);
+        }
+    }
+    
+
+    /* ----------- Screen Position Functions ------------------------------------------------------------- */
+
+    public void UpdatePositions() {
+        /*
+         * Reposition the components of the menu relative to the screen size
+         */
+
+        startButtonRect.position = new Vector3(startButtonRect.sizeDelta.x/2f, canvasRect.position.y, 0);
+    }
+
 
     /* ----------- Listener Functions ------------------------------------------------------------- */
 
@@ -66,6 +164,19 @@ public class Menu : MonoBehaviour {
          * Runs when the user presses the start button
          */
 
+        if(animatingStartButton == false) {
+            animatingStartButton = true;
+            startButtonRemainingTime = 5;
+        }
+
         playerController.StartButtonPressed();
+    }
+
+    public void StartButtonMouseEnter() {
+        /*
+         * The mouse entered the startButton's clickable area
+         */
+
+        Debug.Log("Mouse enterd area");
     }
 }
