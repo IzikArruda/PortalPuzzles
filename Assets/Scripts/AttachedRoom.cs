954b47fec477debae4a3537979cc7be68c1c03f9
IzikArruda <izikarruda@gmail.com>
2018-06-01 22:33:55 -0300

    Revert "GlobalTerrainController now requests each room to change their textures"
    This reverts commit 54027748c8edbd9cfc2eb72cb9f5f7e81c98e246.

--- Assets/Scripts/AttachedRoom.cs
+++ Assets/Scripts/AttachedRoom.cs
@@ -9,9 +9,6 @@ using System.Collections;
 [ExecuteInEditMode]
 public class AttachedRoom : ConnectedRoom {
 
-    /* The globalRoomController which has links to all the rooms of the game */
-    public GlobalRoomController globalRoomController;
-
     /* The two exit points of the room's two exits. Used to connect rooms. */
     public Transform exitPointFront;
     public Transform exitPointBack;
@@ -62,11 +59,6 @@ public class AttachedRoom : ConnectedRoom {
         if(player.GetComponent<CustomPlayerController>() != null) {
             /* Tell the CustomPlayerController to change their linked attachedRoom */
             player.GetComponent<CustomPlayerController>().ChangeLastRoom(this);
-            
-            /* If the player is "falling" backwards through the rooms, send a message to the global room controller */
-            if(player.transform.up == new Vector3(0, 0, 1)) {
-                globalRoomController.UpdateAllRoomTextures(this);
-            }
         }
     }
 
@@ -147,6 +139,16 @@ public class AttachedRoom : ConnectedRoom {
          * Disable the attached puzzle room
          */
          
+
+        /*
+         * 
+         * 
+         * IDEA: instead of disabling the entire room, just set the portal meshes to invisible???
+         * we still want to prevent multiple portal rendering
+         * 
+         * ALSO: have each room start disabled, then have the player send a request to their waitingRoom to start
+         * 
+         */
         if(puzzleRoomParent != null) {
             
             /* Check if the puzzle room uses portals */
