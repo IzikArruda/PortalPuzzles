6d052858f11126c2c48292e98e1ba0dac9c85007
IzikArruda <izikarruda@gmail.com>
2018-06-23 19:55:47 -0300

    Added a scene loader to have a loading screen first
    It takes a while to fully load the main scene of the game, so by adding an empty scene to use as a loading screen, we can load the main scene in the background as the player has some kind of feedback as the game is loading.

---
+++ Assets/StartupTester.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartupTester : MonoBehaviour {
+
+    public TextMesh text;
+    
+	void Start () {
+        text.text = "" + Time.realtimeSinceStartup;
+    }
+
+    void Update() {
+        /*
+         * Reload the testArea on a button press
+         */
+
+        if(Input.GetKeyDown("f")) {
+
+            /* Start loading the new scene */
+            StartCoroutine(LoadGame());
+        }
+
+        /* Update the text with the current time */
+        text.text = "" + Time.realtimeSinceStartup;
+    }
+
+    IEnumerator LoadGame() {
+
+        AsyncOperation async = SceneManager.LoadSceneAsync("TestArea");
+
+        // While the asynchronous operation to load the new scene is not yet complete, continue waiting until it's done.
+        while(!async.isDone) {
+            yield return null;
+        }
+    }
+}
