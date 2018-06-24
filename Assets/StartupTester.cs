7b46fdeb973661edcac9ad7be638f6759dbea54d
IzikArruda <izikarruda@gmail.com>
2018-06-24 13:02:06 -0300

    Added rotation, starting and ending to the loading scene
    The loadingScene's boxes now have a rotation element added to the boxes, which also uses a sepperate step count than the position animation.
    
    The animation also stars with the boxes off-screen, makign them slide into view. Once the game loads (in this case, the right keypress), the boxes will slide out of view to leave the screen white again.

--- Assets/StartupTester.cs
+++ Assets/StartupTester.cs
@@ -17,9 +17,19 @@ public class StartupTester : MonoBehaviour {
 
     /* Values used with timings of the loading animation */
     float startTime = 0;
+    float endTime = -1;
     float startingAnimationTiming = 2;
-    float box2StepCount = 18;
-    float box3StepCount = 7;
+    float endingAnimationTiming = 1.3f;
+    float box1PosStepCount = 60;
+    float box2PosStepCount = 12;
+    float box3PosStepCount = 7;
+    float box1RotStepCount = 7;
+    float box2RotStepCount = 12;
+    float box3RotStepCount = 60;
+    float box1AnimationLoopTime = 2.225f;
+    float box2AnimationLoopTime = 2.25f;
+    float box3AnimationLoopTime = 2.275f;
+    float boxAnimationOffset = -0.05f;
 
 
     /* -------- Built-In Unity Functions ---------------------------------------------------- */
@@ -38,22 +48,28 @@ public class StartupTester : MonoBehaviour {
         /* Check if we want to load the scene. This is used for debugging as we want to stay on this screen */
         if(Input.GetKeyDown("f")) {
             /* Start loading the new scene */
-            StartCoroutine(LoadGame());
+            //StartCoroutine(LoadGame());
+            endTime = Time.realtimeSinceStartup;
         }
 
         /* Position the loading boxes into their default position, relative to the screen size */
         PositionBoxes();
 
 
+        /* Once the scene has finished loading, animate the boxes off-screen */
+        if(endTime > 0) {
+            AnimateBoxesIntro(1 - ((Time.realtimeSinceStartup - endTime) / endingAnimationTiming));
+        }
+
         /* Make sure the boxes start off-screen and animate into view */
-        if(Time.realtimeSinceStartup < startTime + startingAnimationTiming) {
-            AnimateBoxesIntro();
+        else if(Time.realtimeSinceStartup < startTime + startingAnimationTiming) {
+            AnimateBoxesIntro((Time.realtimeSinceStartup - startTime) / startingAnimationTiming);
         }
 
         /* Animate the boxes relative to the current game time */
-        AnimateBox1();
-        AnimateBox2();
-        AnimateBox3();
+        AnimatePositionBox1();
+        AnimatePositionBox2();
+        AnimatePositionBox3();
     }
 
 
@@ -65,82 +81,114 @@ public class StartupTester : MonoBehaviour {
          */
         Vector3 bottomRight = sceneCamera.ViewportToWorldPoint(new Vector3(1, 0, 10));
 
+        /* Reset the rotation of the boxes */
+        loadingBox1.transform.localEulerAngles = new Vector3(0, 0, 0);
+        loadingBox2.transform.localEulerAngles = new Vector3(0, 0, 0);
+        loadingBox3.transform.localEulerAngles = new Vector3(0, 0, 0);
+
         /* Position the boxes */
         loadingBox1.transform.position = bottomRight + new Vector3(-1, 1, 0);
         loadingBox2.transform.position = bottomRight + new Vector3(-3, 1, 0);
         loadingBox3.transform.position = bottomRight + new Vector3(-5, 1, 0);
     }
 
-    void AnimateBoxesIntro() {
+    void AnimateBoxesIntro(float timeFrame) {
         /*
-         * Animate the boxes slidding into view
+         * Animate the boxes slidding into view. The given timeFrame will range from 0 to 1
          */
-        float animationTiming = (Time.realtimeSinceStartup - startTime) / startingAnimationTiming;
         Vector3 animation = Vector3.zero;
         float entryHeight = 5;
 
-        animation = new Vector3(0, -entryHeight + entryHeight*(Mathf.Sin((Mathf.PI/2f)*animationTiming)), 0);
+        animation = new Vector3(0, -entryHeight + entryHeight*(Mathf.Sin((Mathf.PI/2f)*timeFrame)), 0);
         loadingBox1.transform.position += animation;
 
         /* Use steps for the other two cubes */
-        animation = new Vector3(0, -entryHeight + entryHeight*(Mathf.Sin((Mathf.PI/2f)*Mathf.FloorToInt((animationTiming)*box2StepCount)/box2StepCount)), 0);
+        animation = new Vector3(0, -entryHeight + entryHeight*(Mathf.Sin((Mathf.PI/2f)*Mathf.FloorToInt((timeFrame)*box2PosStepCount)/box2PosStepCount)), 0);
         loadingBox2.transform.position += animation;
         
-        animation = new Vector3(0, -entryHeight + entryHeight*(Mathf.Sin((Mathf.PI/2f)*Mathf.FloorToInt((animationTiming)*box3StepCount)/box3StepCount)), 0);
+        animation = new Vector3(0, -entryHeight + entryHeight*(Mathf.Sin((Mathf.PI/2f)*Mathf.FloorToInt((timeFrame)*box3PosStepCount)/box3PosStepCount)), 0);
         loadingBox3.transform.position += animation;
     }
 
-    void AnimateBox1() {
+    void AnimatePositionBox1() {
         /*
-         * Animate box 1 as it moves up and down
+         * Animate box 1 as it moves up and down and rotates. The position and rotation have sepperate step amounts.
          */
-        float animationMaxTime = 1.25f;
-        float time = Time.realtimeSinceStartup % animationMaxTime;
+        float maxTime = box1AnimationLoopTime;
+        float time = Mathf.Sin(Mathf.PI*((Time.realtimeSinceStartup % maxTime)/maxTime));
         Vector3 animation = Vector3.zero;
+        
 
-        animation = new Vector3(0, 1 + Mathf.Cos(Mathf.PI*2*time/animationMaxTime), 0);
-
+        /* Animate the box relative to the time */
+        time = Mathf.FloorToInt((time)*box1PosStepCount)/box1PosStepCount;
+        animation = new Vector3(0, Mathf.Sin(Mathf.PI*time), 0);
         loadingBox1.transform.position += animation;
+
+
+        /* Adjust the rotation of the box relative to the time */
+        time = Mathf.Sin(Mathf.PI*((Time.realtimeSinceStartup % maxTime)/maxTime));
+        time = Mathf.FloorToInt((time)*box1RotStepCount)/box1RotStepCount;
+        float rotationTime = Mathf.Sin(-Mathf.PI*0.025f + Mathf.PI*1.05f*time);
+        if(rotationTime < 0) { rotationTime = 0; }
+        loadingBox1.transform.localEulerAngles += new Vector3(0, 0, 360*rotationTime);
     }
 
-    void AnimateBox2() {
+    void AnimatePositionBox2() {
         /*
-         * Animate box2 moving up and down but with steps in the timing
+         * Animate box2 as it moves up and down and rotates. The position and rotation have sepperate step amounts.
          */
-        float animationMaxTime = 1.25f;
-        float time = Time.realtimeSinceStartup % animationMaxTime;
+        float maxTime = box2AnimationLoopTime;
+        float time = Mathf.Sin(Mathf.PI*(((boxAnimationOffset + Time.realtimeSinceStartup) % maxTime)/maxTime));
         Vector3 animation = Vector3.zero;
 
-        /* Force the timing of this animation to use steps */
-        time = Mathf.FloorToInt((time)*box2StepCount)/box2StepCount;
 
-        animation = new Vector3(0, 1 + Mathf.Cos(Mathf.PI*2*time/animationMaxTime), 0);
-
+        /* Animate the box relative to the time */
+        time = Mathf.FloorToInt((time)*box2PosStepCount*1.01f)/box2PosStepCount;
+        animation = new Vector3(0, Mathf.Sin(Mathf.PI*time), 0);
         loadingBox2.transform.position += animation;
+
+
+        /* Adjust the rotation of the box relative to the time */
+        time = Mathf.Sin(Mathf.PI*(((boxAnimationOffset + Time.realtimeSinceStartup) % maxTime)/maxTime));
+        time = Mathf.FloorToInt((time)*box2RotStepCount*1.01f)/box2RotStepCount;
+        float rotationTime = Mathf.Sin(-Mathf.PI*0.025f + Mathf.PI*1.05f*time);
+        if(rotationTime < 0) { rotationTime = 0; }
+        loadingBox2.transform.localEulerAngles += new Vector3(0, 0, 360*rotationTime);
     }
 
-    void AnimateBox3() {
+    void AnimatePositionBox3() {
         /*
-         * Animate box3 moving up and down but with steps in the timing
+         * Animate box3 as it moves up and down and rotates. The position and rotation have sepperate step amounts.
          */
-        float animationMaxTime = 1.25f;
-        float time = Time.realtimeSinceStartup % animationMaxTime;
+        float maxTime = box3AnimationLoopTime;
+        float time = Mathf.Sin(Mathf.PI*(((2*boxAnimationOffset + Time.realtimeSinceStartup) % maxTime)/maxTime));
         Vector3 animation = Vector3.zero;
+        
 
-        /* Force the timing of this animation to use steps */
-        time = Mathf.FloorToInt((time)*box3StepCount)/box3StepCount;
-
-        animation = new Vector3(0, 1 + Mathf.Cos(Mathf.PI*2*time/animationMaxTime), 0);
-
+        /* Animate the box relative to the time */
+        time = Mathf.FloorToInt((time)*box3PosStepCount*1.01f)/box3PosStepCount;
+        animation = new Vector3(0, Mathf.Sin(Mathf.PI*time), 0);
         loadingBox3.transform.position += animation;
+
+
+        /* Adjust the rotation of the box relative to the time */
+        time = Mathf.Sin(Mathf.PI*(((2*boxAnimationOffset + Time.realtimeSinceStartup) % maxTime)/maxTime));
+        time = Mathf.FloorToInt((time)*box3RotStepCount*1.01f)/box3RotStepCount;
+        float rotationTime = Mathf.Sin(-Mathf.PI*0.025f + Mathf.PI*1.05f*time);
+        if(rotationTime < 0) { rotationTime = 0; }
+        loadingBox3.transform.localEulerAngles += new Vector3(0, 0, 360*rotationTime);
     }
-
+    
     IEnumerator LoadGame() {
-
+        /*
+         * Used to start a coroutine of loading the game's scene
+         */
         AsyncOperation async = SceneManager.LoadSceneAsync("TestArea");
 
-        // While the asynchronous operation to load the new scene is not yet complete, continue waiting until it's done.
+        /* Wait for the scene to be loaded before starting the ending animation by setting endTime */
         while(!async.isDone) {
+            endTime = Time.realtimeSinceStartup;
+            yield return new WaitForSeconds(endingAnimationTiming + 0.25f);
             yield return null;
         }
     }
